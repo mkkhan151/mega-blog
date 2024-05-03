5ed925480adb4bbfdecde5a581ea2db27b9fdaed
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2024-05-03 17:05:37 +0500

    create Home, Login, Signup, Addpost, AllPosts, EditPost and Post pages

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { Container, PostForm } from '../components'
+import dbService from "../appwrite/db";
+import { useNavigate, useParams } from 'react-router-dom';
+
+function EditPost() {
+    const [post, setPost] = useState(null)
+    const { slug } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) {
+            dbService.getPost(slug).then((post) => {
+                if (post) {
+                    setPost(post)
+                }
+            })
+        } else {
+            navigate('/')
+        }
+    }, [slug, navigate])
+    return post ? (
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+    ) : null
+}
+
+export default EditPost
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+import AllPosts from './AllPosts';
+import Post from './Post';
+import AddPost from './AddPost';
+import EditPost from './EditPost'
+
+export { Home, Login, Signup, AllPosts, Post, AddPost, EditPost }
